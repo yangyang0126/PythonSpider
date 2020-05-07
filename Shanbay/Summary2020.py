8fc88d3086295d4d7cc4aee9402fea7d36c19a46
unknown <zhaojingyi0126@163.com>
2020-05-07 08:22:59 +0800

    update

--- Shanbay/Summary2020.py
+++ Shanbay/Summary2020.py
@@ -8,7 +8,8 @@ import requests  # 获取网页数据
 import datetime
 import xlwt
 import pandas as pd
-import pygal  
+import pygal
+  
 # 准备表格
 workbook = xlwt.Workbook()  #定义workbook
 sheet = workbook.add_sheet('2020')  #添加sheet
@@ -63,3 +64,11 @@ MonthDf = df.resample('M').sum().head()
 
 # 绘图
 MonthDf['bdc'][1]
+line_chart = pygal.StackedBar()
+line_chart.title = 'Browser usage evolution (in %)'
+line_chart.x_labels = map(str, range(2002, 2013))
+line_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+line_chart.render_to_file('bar_chart.svg')         
